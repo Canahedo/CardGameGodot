71b5d7575b608acce8d38ad394c567ddfdaac43f
Canahedo <Canahedo@gmail.com>
2025-01-06 22:27:51 -0500

    Starting almost over

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,34 +1,48 @@
 extends Node2D
 
-var card
-
-
-
+var deck_list = []
+var rank_loc_dict = {}
+var suit_loc_dict = {
+	"C":4,
+	"D":148,
+	"S":292,
+	"H":436
+}
+	
+func get_card_cords(rank,suit):
+	var card_x = rank_loc_dict[rank]
+	var card_y = suit_loc_dict[suit]
+	return [card_x,card_y]
+	
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var x = 100
-	await get_tree().create_timer(1).timeout
-	var scene: PackedScene = preload("res://scenes/Card.tscn")
 	
-	var rank = "J"
-	var suit = "S"
+	# Build Deck List
+	for suit in ["C", "D", "S", "H"]:
+		for rank in range(2,11):
+			deck_list.append([str(rank), suit])
+		for rank in ["J", "Q", "K", "A"]:
+			deck_list.append([str(rank), suit])
+			
+	# Builds dict of card rank locations on asset sheet
+	var face_conversion = {
+		0: "A",
+		10: "J",
+		11: "Q",
+		12: "K" 
+	}
+	var x	
+	for i in range(13):
+		if i in face_conversion:
+			x = face_conversion[i]
+		else:
+			x = str(i+1) 	
+		rank_loc_dict[x] = (i * 100) + 4
 	
-	for i in range(5):	
-		var instance = scene.instantiate()
-		instance.position = Vector2(i * x, 0)
-		
-		var card_sprite = $Deck.get_card_cords(rank,suit)
-		instance.card_sprite_x = card_sprite[0]
-		instance.card_sprite_y = card_sprite[1]
-		add_child(instance)
-		await get_tree().create_timer(1).timeout
-	
-	
-	pass # Replace with function body.
-
+	# Test
+	$Sprite2D.region_rect = Rect2(rank_loc_dict["A"] ,suit_loc_dict["S"] , 92, 136)	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
